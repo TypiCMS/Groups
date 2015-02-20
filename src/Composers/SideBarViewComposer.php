caba5d5266abac4c79d96228472afc94f59bb906
Samuel De Backer <sdebacker@gmail.com>
2015-02-20 01:31:47 +0100

    licence + sidebar

--- src/Composers/SideBarViewComposer.php
+++ src/Composers/SideBarViewComposer.php
@@ -1,18 +1,25 @@
 <?php
 namespace TypiCMS\Modules\Groups\Composers;
 
-use Illuminate\View\View;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Sidebar\SidebarGroup;
+use Maatwebsite\Sidebar\SidebarItem;
+use TypiCMS\Composers\BaseSidebarViewComposer;
 
-class SidebarViewComposer
+class SidebarViewComposer extends BaseSidebarViewComposer
 {
     public function compose(View $view)
     {
-        $view->menus['users']->put('groups', [
-            'weight' => config('typicms.groups.sidebar.weight'),
-            'request' => $view->prefix . '/groups*',
-            'route' => 'admin.groups.index',
-            'icon-class' => 'icon fa fa-fw fa-user',
-            'title' => 'Groups',
-        ]);
+        $view->sidebar->group(trans('global.menus.users'), function (SidebarGroup $group) {
+            $group->addItem(trans('groups::global.name'), function (SidebarItem $item) {
+                $item->icon = config('typicms.groups.sidebar.icon', 'icon fa fa-fw fa-users');
+                $item->weight = config('typicms.groups.sidebar.weight');
+                $item->route('admin.groups.index');
+                $item->append('admin.groups.create');
+                $item->authorize(
+                    $this->auth->hasAccess('groups.index')
+                );
+            });
+        });
     }
 }
