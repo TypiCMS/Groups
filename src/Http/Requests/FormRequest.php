015bea2fc776b821aa9480f66bf5273c54227550
Samuel De Backer <sdebacker@gmail.com>
2015-03-12 23:33:33 +0100

    validation

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -8,7 +8,7 @@ class FormRequest extends AbstractFormRequest {
     public function rules()
     {
         $rules = [
-            'name' => 'required|min:4|unique:groups,name,' . $this->get('id'),
+            'name' => 'required|min:4|unique:groups,name,' . $this->id,
         ];
         return $rules;
     }
