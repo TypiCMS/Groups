972485d3047001c38451279a5e210604c0fca095
Samuel De Backer <sdebacker@gmail.com>
2015-03-18 23:11:42 +0100

    _index.blade

--- src/resources/views/admin/_index.blade.php
+++ src/resources/views/admin/_index.blade.php
@@ -1,8 +1,8 @@
 <div ng-app="typicms" ng-cloak ng-controller="ListController">
 
     <h1>
-        <a href="{{ url }}/create" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only" translate>New</span></a>
-        <span translate translate-n="models.length" translate-plural="{{ models.length }} groups">{{ models.length }} group</span>
+        <a href="{{ route('admin.' . $module . '.create') }}" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only" translate>New</span></a>
+        <span translate translate-n="models.length" translate-plural="@{{ models.length }} groups">@{{ models.length }} group</span>
     </h1>
 
     <div class="btn-toolbar" role="toolbar" ng-include="'/views/partials/btnLocales.html'"></div>
@@ -20,10 +20,10 @@
                 <tr>
                     <td colspan="2"></td>
                     <td>
-                        <input st-search="name" class="form-control input-sm" placeholder="{{ 'Search' | translate }}…" type="text">
+                        <input st-search="name" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
                     </td>
                     <td>
-                        <input st-search="permissions" class="form-control input-sm" placeholder="{{ 'Search' | translate }}…" type="text">
+                        <input st-search="permissions" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
                     </td>
                 </tr>
             </thead>
@@ -32,8 +32,8 @@
                 <tr ng-repeat="model in displayedModels">
                     <td><typi-btn-delete ng-click="delete(model, model.name)"></typi-btn-delete></td>
                     <td typi-btn-edit></td>
-                    <td>{{ model.name }}</td>
-                    <td>{{ model.permissions }}</td>
+                    <td>@{{ model.name }}</td>
+                    <td>@{{ model.permissions }}</td>
                 </tr>
             </tbody>
             <tfoot>
