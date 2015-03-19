df0028ed8342bea37fe1f9343ca9ccd8572c2895
Samuel De Backer <sdebacker@gmail.com>
2015-03-19 21:03:59 +0100

    ng directives

--- src/resources/views/admin/_index.blade.php
+++ src/resources/views/admin/_index.blade.php
@@ -30,8 +30,10 @@
 
             <tbody>
                 <tr ng-repeat="model in displayedModels">
-                    <td><typi-btn-delete ng-click="delete(model, model.name)"></typi-btn-delete></td>
-                    <td typi-btn-edit></td>
+                    <td typi-btn-delete action="delete(model, model.name)"></td>
+                    <td>
+                        @include('core::admin._button-edit')
+                    </td>
                     <td>@{{ model.name }}</td>
                     <td>@{{ model.permissions }}</td>
                 </tr>
