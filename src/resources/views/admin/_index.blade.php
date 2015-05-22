340fcd793496c929c4fc3aca54f723bf3ad91686
Samuel De Backer <sdebacker@gmail.com>
2015-05-22 22:32:08 +0200

    angular gettext removed

--- src/resources/views/admin/_index.blade.php
+++ src/resources/views/admin/_index.blade.php
@@ -1,8 +1,8 @@
 <div ng-app="typicms" ng-cloak ng-controller="ListController">
 
     <h1>
-        <a href="{{ route('admin.' . $module . '.create') }}" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only" translate>New</span></a>
-        <span translate translate-n="models.length" translate-plural="@{{ models.length }} groups">@{{ models.length }} group</span>
+        <a href="{{ route('admin.' . $module . '.create') }}" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only">New</span></a>
+        <span>@{{ models.length }} @choice('groups::global.groups', 2)</span>
     </h1>
 
     <div class="btn-toolbar" role="toolbar" ng-include="'/views/partials/btnLocales.html'"></div>
@@ -14,16 +14,16 @@
                 <tr>
                     <th class="delete"></th>
                     <th class="edit"></th>
-                    <th st-sort="name" class="name st-sort" translate>Name</th>
-                    <th st-sort="permissions" class="permissions st-sort" translate>Permissions</th>
+                    <th st-sort="name" class="name st-sort">Name</th>
+                    <th st-sort="permissions" class="permissions st-sort">Permissions</th>
                 </tr>
                 <tr>
                     <td colspan="2"></td>
                     <td>
-                        <input st-search="name" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
+                        <input st-search="name" class="form-control input-sm" placeholder="@lang('global.Search')…" type="text">
                     </td>
                     <td>
-                        <input st-search="permissions" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
+                        <input st-search="permissions" class="form-control input-sm" placeholder="@lang('global.Search')…" type="text">
                     </td>
                 </tr>
             </thead>
