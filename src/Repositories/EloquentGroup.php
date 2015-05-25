0ad521cdf05e99b0f066d65d0fde619eefe4c544
Samuel De Backer <sdebacker@gmail.com>
2015-05-25 17:34:42 +0200

    groups save

--- src/Repositories/EloquentGroup.php
+++ src/Repositories/EloquentGroup.php
@@ -24,4 +24,67 @@ class EloquentGroup extends RepositoriesAbstract implements GroupInterface
         return $this->make($with)->order()->get();
     }
 
+    /**
+     * Create a new model
+     *
+     * @param  array $data
+     * @return mixed Model or false on error during save
+     */
+    public function create(array $data)
+    {
+        $model = $this->model;
+
+        $groupData = array_except($data, ['_method','_token', 'id', 'exit']);
+        $groupData['permissions'] = $this->permissions($data);
+
+        foreach ($groupData as $key => $value) {
+            $model->$key = $value;
+        }
+
+        if ($model->save()) {
+            return $model;
+        }
+
+        return false;
+    }
+
+    /**
+     * Update an existing model
+     *
+     * @param  array  $data
+     * @return boolean
+     */
+    public function update(array $data)
+    {
+        $group = $this->model->find($data['id']);
+
+        $groupData = array_except($data, ['_method', '_token', 'exit']);
+        $groupData['permissions'] = $this->permissions($data);
+
+        foreach ($groupData as $key => $value) {
+            $group->$key = $value;
+        }
+
+        if ($group->save()) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    /**
+     * get extract and encode permissions from array
+     *
+     * @param  array $data
+     * @return string|null
+     */
+    private function permissions($data)
+    {
+        if (isset($data['permissions'])) {
+            return json_encode($data['permissions']);
+        }
+        return null;
+    }
+
 }
